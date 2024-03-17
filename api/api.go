df14e7c2e0b6e72db2474117c41ae348bffd9da7
Nikx908 <nikhilparbat908@gmail.com>
2024-03-17 15:54:17 +0530

    cors edited

--- api/api.go
+++ api/api.go
@@ -6,26 +6,19 @@ import (
 	"github.com/NikhilParbat/CC-Compiler-Go/controllers"
 )
 
-// handler function to handle HTTP requests
+// Handler function to handle HTTP requests
 func Handler(w http.ResponseWriter, r *http.Request) {
+	// Set headers to allow cross-origin requests
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+	// Execute the actual logic of the Handler
 	controllers.ExecuteCodeHandler(w, r)
-}
-
-// CORS middleware function to set necessary headers
-func cors(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Set headers to allow cross-origin requests
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-		// Continue with the next handler
-		next(w, r)
-	}
 }
 
 func init() {
 	// Start the serverless function
-	http.HandleFunc("/execute", cors(Handler))
+	http.HandleFunc("/execute", Handler)
 	http.ListenAndServe(":8080", nil)
 }
