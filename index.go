3af81f1a6b867db2a7e116458f43ac219710415e
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-17 15:22:24 +0530

    add cors

--- index.go
+++ index.go
@@ -7,6 +7,19 @@ import (
 	"github.com/NikhilParbat/CC-Compiler-Go/controllers"
 )
 
+// CORS middleware function to set necessary headers
+func CORS(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Set headers to allow cross-origin requests
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Continue with the next handler
+		next(w, r)
+	}
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 	// Define your route handlers
 	switch r.URL.Path {
@@ -19,5 +32,5 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// Start the serverless function
-	http.HandleFunc("/", Handler)
+	http.HandleFunc("/", CORS(Handler))
 }
